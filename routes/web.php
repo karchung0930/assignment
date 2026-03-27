ed17e5d554744626a4e16c984dcf5e22498c38e3
ace <karchung0930@pm.me>
2026-03-27 13:52:48 +0800

    feat: Add a root route.

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\MemberSpendingController;
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return redirect('/login');
+});
+
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'create'])->name('login');
     Route::post('/login', [AuthController::class, 'store']);
